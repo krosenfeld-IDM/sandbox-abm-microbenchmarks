7cfdf6ff3ff38fe47fa86a13fd98a13f69192e0a
github-action-benchmark <github@users.noreply.github.com>
2024-02-09 15:30:05 +0000

    add Julia Benchmark (julia) benchmark result for bfa2e01b80e19fe6ef1423907a2de71d958f2db7

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1707492605625,
+  "repoUrl": "https://github.com/krosenfeld-IDM/sandbox-abm-microbenchmarks",
+  "entries": {
+    "Julia Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "56368533+krosenfeld-IDM@users.noreply.github.com",
+            "name": "Katherine Rosenfeld",
+            "username": "krosenfeld-IDM"
+          },
+          "committer": {
+            "email": "56368533+krosenfeld-IDM@users.noreply.github.com",
+            "name": "Katherine Rosenfeld",
+            "username": "krosenfeld-IDM"
+          },
+          "distinct": true,
+          "id": "bfa2e01b80e19fe6ef1423907a2de71d958f2db7",
+          "message": "change package to project",
+          "timestamp": "2024-02-09T15:28:16Z",
+          "tree_id": "cfd9f9c81ededf15b73c25678fea1f89666c7e41",
+          "url": "https://github.com/krosenfeld-IDM/sandbox-abm-microbenchmarks/commit/bfa2e01b80e19fe6ef1423907a2de71d958f2db7"
+        },
+        "date": 1707492604740,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Array Access/Ordered.Int8",
+            "value": 7739181.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Array Access/Random.Int64",
+            "value": 46664267,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Array Access/Ordered.Int64",
+            "value": 7859560.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Array Access/Random.Int8",
+            "value": 15951764,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
