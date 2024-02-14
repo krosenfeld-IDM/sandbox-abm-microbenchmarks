2916bd3a987a38eb3ffb5624deef109a7ce43ff2
Katherine Rosenfeld <krosenf@gmail.com>
2024-02-14 00:54:44 +0000

    fix relative directory

--- .github/run-arrays.sh
+++ .github/run-arrays.sh
@@ -2,14 +2,14 @@
 
 (
 echo "Benchmarking numpy"
-python arrays/numpy/benchmark_arrays.py
+python benchmarks/arrays/numpy/benchmark_arrays.py
 
 echo "Benchmarking julia"
-julia --project=@. arrays/julia/benchmark_arrays.jl
+julia --project=@. benchmarks/arrays/julia/benchmark_arrays.jl
 
 echo "Benchmarking c-extensions"
-python arrays/c-extension/benchmark_arrays.py
+python benchmarks/arrays/c-extension/benchmark_arrays.py
 
 ) | tee benchmark_results.txt
 
-julia --project=@. create_benchmark_table.jl
+julia --project=@. benchmarks/create_benchmark_table.jl
