a14ce31d8abbdd53feec9ea7344f251552cad9be
Katherine <krosenf@gmail.com>
2024-02-13 21:44:54 -0800

    Use README in .github

---
+++ .github/README.md
@@ -0,0 +1,8 @@
+# sandbox-abm-microbenchmarks
+Sandbox for microbenchmarks
+
+## Results
+<!--dynamic-content-->
+
+**References**
+- https://github.com/JuliaDynamics/ABMFrameworksComparison/tree/main
--- .github/update_readme.sh
+++ .github/update_readme.sh
@@ -7,5 +7,5 @@ julia --project=@. benchmarks/create_benchmark_table.jl > table.txt
 awk -v content="$(<table.txt)" '{
     gsub(/<!--dynamic-content-->/, content);
     print;
-}' README.md > temp.txt 
+}' .github/README.md > temp.txt 
 mv temp.txt README.md
